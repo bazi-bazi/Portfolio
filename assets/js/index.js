9e1679787003db6dca8b7067e343112e00421c11
Vazha Davituliani <vazha.davituliani@gmail.com>
2020-02-12 23:01:06 +0400

    add vue text form types

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,29 @@
 
 
-var test = new Vue({
+var name = new Vue({
     el: '#form-name',
     data: {
         name: ''
   }
+});
+
+var email = new Vue({
+    el: '#form-email',
+    data: {
+        email: ''
+    }
+});
+
+var title = new Vue({
+    el: '#form-title',
+    data: {
+        title:''
+    }
+});
+
+var textarea = new Vue({
+    el: '#form-comment',
+    data: {
+        textarea: ''
+    }
 });
